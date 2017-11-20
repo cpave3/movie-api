8ba0ea3e97d06bd579b8d4a91b5de76837b5a89e
Cameron Pavey <cpave3@gmail.com>
2017-11-20 12:38:32 +1000

    updated installer

--- install.sh
+++ install.sh
@@ -6,4 +6,7 @@ echo "[*] Installing PHP dependencies"
 docker run --rm --interactive --tty --volume $PWD:/app composer install
 echo "[*] Installing Database Migrations"
 docker-compose exec app php artisan migrate --seed
+docker-compose exec app php artisan key:generate
+chmod -R 777 storage/
+chmod -R 777 public/
 echo "[*] Installation Complete"
