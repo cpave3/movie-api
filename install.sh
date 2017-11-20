fddf75ac5e8e01ef5e47d8cf6b112dcadc3bcbc1
Cameron Pavey <cpave3@gmail.com>
2017-11-20 10:42:51 +1000

    added installer

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "[*] Bringing containers online"
+docker-compose up -d
+echo "[*] Installing PHP dependencies"
+docker run --rm --interactive --tty --volume $PWD:/app composer install
+echo "[*] Installing Database Migrations"
+docker-compose exec app php artisan migrate --seed
+echo "[*] Installation Complete"
